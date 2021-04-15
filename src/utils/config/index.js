1ed1ab87f66ab36e57bc9f9286174d56484ecfe7
Willian Amaral <willian.amaral@outlook.com.br>
2021-04-15 18:03:24 -0300

    Initialize project

---
+++ src/utils/config/index.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyC3newJJWmsY7-W0777kOeE_Hq9WSVCFSI',
+  authDomain: 'shout-motion.firebaseapp.com',
+  projectId: 'shout-motion',
+  storageBucket: 'shout-motion.appspot.com',
+  messagingSenderId: '723876090231',
+  appId: '1:723876090231:web:5265ca5f8c075c1a7b5172',
+  measurementId: 'G-815C6M5QHP',
+};
