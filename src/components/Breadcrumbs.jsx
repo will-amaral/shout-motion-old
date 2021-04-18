f520d42978c523de15754787ab9cb79515deed1e
Willian Amaral <willian.amaral@outlook.com.br>
2021-04-18 17:32:06 -0300

    Breadcrumb with current

--- src/components/Breadcrumbs.jsx
+++ src/components/Breadcrumbs.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import { Link as RouterLink } from 'react-router-dom';
-import { Breadcrumbs as MuiBreadcrumbs, Link } from '@material-ui/core';
+import { Breadcrumbs as MuiBreadcrumbs, Link, Typography } from '@material-ui/core';
 import { ChevronRight } from 'components/icons';
 
 function Breadcrumbs(props) {
@@ -11,11 +11,26 @@ function Breadcrumbs(props) {
       separator={<ChevronRight fontSize='small' />}
       sx={{ mt: 1 }}
     >
-      {paths.map((path) => (
-        <Link color='textPrimary' variant='subtitle2' component={RouterLink} to={path.to}>
-          {path.text}
-        </Link>
-      ))}
+      {paths.map((path, index) => {
+        if (index === paths.length - 1) {
+          return (
+            <Typography key={index} color='textSecondary' variant='subtitle2'>
+              {path.text}
+            </Typography>
+          );
+        } else
+          return (
+            <Link
+              key={index}
+              color='textPrimary'
+              variant='subtitle2'
+              component={RouterLink}
+              to={path.to}
+            >
+              {path.text}
+            </Link>
+          );
+      })}
     </MuiBreadcrumbs>
   );
 }
