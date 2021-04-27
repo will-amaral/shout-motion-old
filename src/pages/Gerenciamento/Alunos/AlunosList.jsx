3a93e0c96a158b8dd9311ec4435b5d8a477822fe
Willian Amaral <willian.amaral@outlook.com.br>
2021-04-27 00:00:11 -0300

    Add hook to list

--- src/pages/Gerenciamento/Alunos/AlunosList.jsx
+++ src/pages/Gerenciamento/Alunos/AlunosList.jsx
@@ -1,30 +1,16 @@
-import { useState, useEffect } from 'react';
 import { Button } from '@material-ui/core';
 import { Link } from 'react-router-dom';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
 import { Wrapper, Header, Breadcrumbs, Table } from 'components';
 import { UserAdd } from 'components/icons';
 import { db } from 'utils/lib/firebase';
 import columns from './columns';
 
 function AlunosList() {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const getData = async () => {
-      setLoading(true);
-      const arr = [];
-      const query = await db
-        .collection('Users')
-        .where('role', '==', 'Aluno')
-        .where('active', '==', true)
-        .get();
-      query.forEach((doc) => arr.push({ id: doc.id, ...doc.data() }));
-      setData(arr);
-      setLoading(false);
-    };
-    getData();
-  }, []);
+  const [data, loading] = useCollectionData(
+    db.collection('Users').where('role', '==', 'Aluno').where('active', '==', true),
+    { idField: 'id' }
+  );
 
   return (
     <Wrapper title='Alunos | ShoutMotion'>
@@ -48,7 +34,7 @@ function AlunosList() {
           ]}
         />
       </Header>
-      <Table rows={data} columns={columns} rowHeight={75} loading={loading} />
+      <Table rows={data ? data : []} columns={columns} rowHeight={75} loading={loading} />
     </Wrapper>
   );
 }
