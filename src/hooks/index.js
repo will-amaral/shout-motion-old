57ec6f610aeb6ac0efe7b2cd281b368d31e5fa43
Willian Amaral <willian.amaral@outlook.com.br>
2021-07-04 20:53:02 -0300

    Move to nextjs

--- src/hooks/index.js
+++
@@ -1,6 +0,0 @@
-export { default as useAuth } from './useAuth';
-export { default as useIsMountedRef } from './useIsMountedRef';
-export { default as useIsDark } from './useIsDark';
-export { default as useNotification } from './useNotification';
-export { default as useScrollReset } from './useScrollReset';
-export { default as useSettings } from './useSettings';
--- src/hooks/useIsDark.js
+++
@@ -1,9 +0,0 @@
-import useSettings from './useSettings';
-import { THEMES } from 'utils/constants';
-
-const useIsDark = () => {
-  const { settings } = useSettings();
-  return Boolean(settings.theme === THEMES.DARK || settings.theme === THEMES.ALT_DARK);
-};
-
-export default useIsDark;
